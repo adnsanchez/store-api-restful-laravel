f40981b50ae918310c3b9c5d4353a8df3a3cec2f
Adrian Sanchez <asanchez@mexicocarrental.com.mx>
2020-06-12 09:41:17 -0500

    first-commit store api restful

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json(['data' => $users], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed'
+        ];
+
+        $this->validate($request, $rules);
+
+        $fields = $request->all();
+
+        $fields['password'] = bcrypt($request->password);
+        $fields['verified'] = User::NOT_VERIFIED;
+        $fields['verification_token'] = User::generateVerificationToken();
+        $fields['admin'] = User::NOT_ADMIN;
+
+        $user = User::create($fields);
+
+        return response()->json(['data' => $user], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        return response()->json(['data' => $user], 200);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $rules = [
+            'email' => 'email|unique:users,email,' . $user->id,
+            'password' => 'min:6|confirmed',
+            'admin' => 'in:' . User::ADMIN . ',' . User::NOT_ADMIN,
+        ];
+        
+        $this->validate($request, $rules);
+        
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+        if ($request->has('email') && $user->email != $request->email) {
+            $user->verified = User::NOT_VERIFIED;
+            $user->verification_token = User::generateVerificationToken();
+            $user->email = $request->email;
+        }
+
+
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        if ($request->has('admin')) {
+            
+            if (!$user->isVerified()) {
+                return response()->json(['error' => 'Only verified users can change the admin value', 'code' => 409], 409);
+            }
+
+            $user->admin = $request->admin;
+        }
+
+        if (!$user->isDirty()) {
+            return response()->json(['error' => 'Request almost one different value to update', 'code' => 422], 422);
+        }
+
+        $user->save();
+
+        return response()->json(['data' => $user], 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->delete();
+
+        return response()->json(['data' => $user], 200);
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    const VERIFIED = '1';
+    const NOT_VERIFIED = '0';
+
+    const ADMIN = 'true';
+    const NOT_ADMIN = 'false';
+
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 
+        'email', 
+        'password',
+        'verified',
+        'verification_token',
+        'admin',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 
+        'remember_token',
+        'verification_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function isVerified()
+    {
+        return $this->verified == User::VERIFIED;
+    }
+
+    public function isAdmin()
+    {
+        return $this->admin == User::ADMIN;
+    }
+
+    public static function generateVerificationToken()
+    {
+        return Str::random(40);
+    }
+}
